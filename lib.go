7dcd8a948bf24c4f045e4ef7c0f6f768ff51a9d2
Ted Brownlow <ted.brownlow@gmail.com>
2022-12-09 12:39:16 -0500

    Run function

--- lib.go
+++ lib.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/mattn/go-gtk/glib"
 	"github.com/mattn/go-gtk/gtk"
 )
 
-func widgetForFlag(f *flag.Flag) gtk.IWidget {
+type boolFlag interface {
+	flag.Value
+	IsBoolFlag() bool
+}
+
+func widgetForStringFlag(
+	f *flag.Flag,
+) (gtk.IWidget, func() string) {
 	widgetV := gtk.NewVBox(false, 0)
 	label := gtk.NewLabel(f.Name)
-	label.ModifyFontEasy("bold")
+	label.ModifyFontEasy("bold 24px")
 	widgetV.Add(label)
 	widget := gtk.NewHBox(false, 0)
 	entry := gtk.NewEntry()
@@ -21,8 +29,6 @@ func widgetForFlag(f *flag.Flag) gtk.IWidget {
 
 	button := gtk.NewButtonWithLabel("Choose File/Folder")
 	button.Clicked(func() {
-		println("Dialog OK!")
-
 		//--------------------------------------------------------
 		// GtkFileChooserDialog
 		//--------------------------------------------------------
@@ -32,9 +38,10 @@ func widgetForFlag(f *flag.Flag) gtk.IWidget {
 			gtk.FILE_CHOOSER_ACTION_OPEN,
 			gtk.STOCK_OK,
 			gtk.RESPONSE_ACCEPT)
-		filter := gtk.NewFileFilter()
-		filter.AddPattern("*")
-		filechooserdialog.AddFilter(filter)
+		// filter := gtk.NewFileFilter()
+		// filter.SetName("")
+		// filter.AddPattern("*")
+		// filechooserdialog.AddFilter(filter)
 		filechooserdialog.Response(func() {
 			entry.SetText(filechooserdialog.GetFilename())
 			filechooserdialog.Destroy()
@@ -46,18 +53,58 @@ func widgetForFlag(f *flag.Flag) gtk.IWidget {
 	desc := gtk.NewLabel(f.Usage)
 	widgetV.Add(desc)
 
-	//f.Value
-
-	return widgetV
+	return widgetV, entry.GetText
 }
 
-func RunGui() {
+func widgetForBoolFlag(
+	f *flag.Flag, value boolFlag,
+) (gtk.IWidget, func() string) {
+	widgetV := gtk.NewVBox(false, 0)
+	widgetH := gtk.NewHBox(false, 0)
+
+	label := gtk.NewLabel(f.Name)
+	label.ModifyFontEasy("bold 24px")
+
+	widgetV.PackStart(widgetH, false, false, 0)
+
+	checkButton := gtk.NewCheckButton()
+	widgetH.PackStart(checkButton, false, false, 20)
+	widgetH.PackStart(label, false, false, 0)
+
+	descRow := gtk.NewHBox(false, 0)
+	widgetV.PackStart(descRow, false, false, 0)
+	descRow.PackStart(gtk.NewLabel(f.Usage), false, false, 20)
+
+	getter := func() string {
+		if checkButton.GetActive() {
+			return "true"
+		} else {
+			return "false"
+		}
+	}
+
+	return widgetV, getter
+}
+
+func widgetForFlag(f *flag.Flag) (gtk.IWidget, func() string) {
+	bf, ok := f.Value.(boolFlag)
+	if ok && bf.IsBoolFlag() {
+		return widgetForBoolFlag(f, bf)
+	}
+
+	return widgetForStringFlag(f)
+}
+
+type FlagValueGetters map[*flag.Flag]func() string
+
+func RunGui(appName string, exec func() error) {
+	flagValueGetters := make(FlagValueGetters)
+
 	//var menuitem *gtk.MenuItem
 	gtk.Init(nil)
 	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
 	window.SetPosition(gtk.WIN_POS_CENTER)
-	window.SetTitle("GTK Go!")
-	window.SetIconName("gtk-dialog-info")
+	window.SetTitle(appName)
 	window.Connect("destroy", func(ctx *glib.CallbackContext) {
 		println("got destroy!", ctx.Data().(string))
 		gtk.MainQuit()
@@ -91,15 +138,15 @@ func RunGui() {
 	//framebox1.Add(gtk.NewFixed(0, 1, 1, 1))
 	frame1.Add(framebox1)
 
-	frame2 := gtk.NewFrame("Output")
+	frame2 := gtk.NewFrame("Execute")
 	framebox2 := gtk.NewVBox(false, 1)
 	frame2.Add(framebox2)
 
 	//runBoxV := gtk.NewVBox(false, 0)
 	runBoxH := gtk.NewHBox(false, 0)
-	runLabel := gtk.NewButtonWithLabel("Run")
+	runButton := gtk.NewButtonWithLabel("Run")
 	runBoxH.Add(gtk.NewHBox(false, 0))
-	runBoxH.Add(runLabel)
+	runBoxH.Add(runButton)
 	runBoxH.Add(gtk.NewHBox(false, 0))
 	framebox2.PackStart(runBoxH, false, false, 0)
 
@@ -112,15 +159,52 @@ func RunGui() {
 	})
 
 	for idx, someFlag := range flags {
-		for c := 0; c < 10; c++ {
-			widget := widgetForFlag(someFlag)
-			framebox1.PackStart(widget, false, false, 10)
+		widget, getter := widgetForFlag(someFlag)
+		flagValueGetters[someFlag] = getter
 
-			if idx+1 < len(flags)-1 {
-				//framebox1.PackStart(gtk.NewVSeparator(), true, false, 5)
-			}
+		framebox1.PackStart(widget, false, false, 5)
+
+		if idx+1 < len(flags)-1 {
+			// sepBox := gtk.NewVBox(false, 20)
+			// sep := gtk.NewHSeparator()
+			// sepBox.Add(sep)
+			// framebox1.PackStart(sepBox, false, true, 120)
 		}
 	}
+
+	runButton.Clicked(func() {
+		// Set flags
+		for _, flag := range flags {
+			getter, ok := flagValueGetters[flag]
+			if !ok {
+				panic("Could not find getter for flag " + flag.Name)
+			}
+			err := flag.Value.Set(getter())
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		}
+
+		// Execute program.
+		err := exec()
+
+		mt := gtk.MESSAGE_INFO
+		msg := "Operation executed succesfully"
+		if err != nil {
+			msg = fmt.Sprintf("Error: %s", err.Error())
+			mt = gtk.MESSAGE_ERROR
+		}
+
+		dlg := gtk.NewMessageDialog(
+			window, gtk.DIALOG_DESTROY_WITH_PARENT, mt, gtk.BUTTONS_OK,
+			msg,
+		)
+		dlg.Response(func() {
+			dlg.Destroy()
+		})
+		dlg.Show()
+	})
 
 	//--------------------------------------------------------
 	// Event
--- main.go
+++ main.go
@@ -2,47 +2,21 @@ package main
 
 import (
 	"flag"
-	"os/exec"
-	"regexp"
-	"sort"
-	"strings"
+	"fmt"
 )
-
-func uniq(strings []string) (ret []string) {
-	return []string{}
-}
-
-func authors() []string {
-	if b, err := exec.Command("git", "log").Output(); err == nil {
-		lines := strings.Split(string(b), "\n")
-
-		var a []string
-		r := regexp.MustCompile(`^Author:\s*([^ <]+).*$`)
-		for _, e := range lines {
-			ms := r.FindStringSubmatch(e)
-			if ms == nil {
-				continue
-			}
-			a = append(a, ms[1])
-		}
-		sort.Strings(a)
-		var p string
-		lines = []string{}
-		for _, e := range a {
-			if p == e {
-				continue
-			}
-			lines = append(lines, e)
-			p = e
-		}
-		return lines
-	}
-	return []string{"Yasuhiro Matsumoto <mattn.jp@gmail.com>"}
-}
 
 var nameFlag = flag.String("name", "example name", "choose a name!!!")
 var reverseFlag = flag.Bool("reverse", false, "should we reverse?")
+var x = flag.CommandLine.Int64("number", 23, "set a number :)")
 
 func main() {
-	RunGui()
+	RunGui("My custom application", doStuff)
+}
+
+func doStuff() error {
+	if *reverseFlag {
+		return fmt.Errorf("Reverse not supported")
+	}
+	fmt.Println(*nameFlag, *reverseFlag, *x)
+	return nil
 }
